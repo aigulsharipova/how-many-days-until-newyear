a1a9090ffdf3a03f3feca2258ffbeb47a17d0af2
Aigul <sharipova.aig@gmail.com>
2022-07-06 21:38:33 -0400

    calculate ms

--- script.js
+++ script.js
@@ -2,4 +2,9 @@ function newYear() {
   const newYearDate = new Date('January 01, 2023, 00:00');
   const now = new Date();
   const diff = newYearDate - now;
+
+  const msInSecond = 1000;
+  const msInMinute = 60 * 1000;
+  const msInHour = 60 * 60 * 1000;
+  const msInDay = 24 * 60 * 60 * 1000;
 }
