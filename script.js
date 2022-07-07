891c5977dd7fc9911eea7c4ec2be724366aa4b8f
Aigul <sharipova.aig@gmail.com>
2022-07-06 21:44:45 -0400

    calculate timer

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function newYear() {
+function newYearTimer() {
   const newYearDate = new Date('January 01, 2023, 00:00');
   const now = new Date();
   const diff = newYearDate - now;
@@ -7,4 +7,43 @@ function newYear() {
   const msInMinute = 60 * 1000;
   const msInHour = 60 * 60 * 1000;
   const msInDay = 24 * 60 * 60 * 1000;
-}
+
+  const displayDay = Math.floor(diff/msInDay);
+  document.querySelector('.days').textContent = displayDay;
+
+  const displayHours = Math.floor((diff%msInDay) / msInHour);
+  document.querySelector('.hours').textContent = displayHours;
+
+  const displayMin = Math.floor((diff%msInHour) / msInMinute);
+  document.querySelector('.minutes').textContent = displayMin;
+
+  const displaySec = Math.floor((diff%msInMinute) / msInSecond);
+  document.querySelector('.seconds').textContent = displaySec;
+
+  if (diff <= 0) {
+    document.querySelector('.days').textContent = 0;
+    document.querySelector('.hours').textContent = 0;
+    document.querySelector('.minutes').textContent = 0;
+    document.querySelector('.seconds').textContent = 0;
+    clearInterval(timerID);
+    newYear();
+  }
+  }
+  
+  let timerID = setInterval(newYearTimer, 1000)
+  
+  function newYear() {
+    const heading = document.querySelector('h1');
+    heading.textContent = "HAPPY NEW YEAR!!!";
+    heading.classList.add('red');
+  }
+  
+  const button = document.querySelector('#myButton');
+  button.addEventListener('click', function() {
+    document.querySelector('#myAudio').play()
+  })
+  
+  const buttonOff = document.querySelector('#myButton');
+  button.addEventListener('dblclick', function() {
+    document.querySelector('#myAudio').pause()
+  })
